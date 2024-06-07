b3537325fe5fee53dc9d798eec67290c000d43ff
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2024-06-07 16:04:56 -0300

    refactor: complete class ParteContraria

--- src/main/java/facs/eng/ParteContraria.java
+++ src/main/java/facs/eng/ParteContraria.java
@@ -6,4 +6,33 @@ public class ParteContraria {
     private String nome;
     private String cpfCnpj;
 
+    public ParteContraria(String numProcesso, String nome, String cpfCnpj) {
+        this.numProcesso = numProcesso;
+        this.nome = nome;
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getNumProcesso() {
+        return numProcesso;
+    }
+
+    public void setNumProcesso(String numProcesso) {
+        this.numProcesso = numProcesso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfCnpj() {
+        return this.cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
 }
